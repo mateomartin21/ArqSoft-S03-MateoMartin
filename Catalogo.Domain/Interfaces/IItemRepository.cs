85f3f5bca01e2653f68f1d41f3c9d228773183e6
mateomartin21 <mateo.martin@tecdesoftware.edu.mx>
2026-05-19 13:12:33 -0600

    CREATE: Domain-Item, ItemRepository

---
+++ Catalogo.Domain/Interfaces/IItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using CatalogoApp.Domain.Models;
+
+namespace CatalogoApp.Domain.Interfaces
+{
+    public interface IItemRepository
+    {
+        List<Item> ObtenerTodos();
+        Item? ObtenerPorId(int id);
+        void Agregar(Item item);
+        void Eliminar(int id);
+    }
+}
